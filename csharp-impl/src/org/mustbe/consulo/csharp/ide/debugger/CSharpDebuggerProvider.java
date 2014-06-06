3b90bf12a8aaae0220835266c2a4758437ddb539
VISTALL <vistall.valeriy@gmail.com>
2014-06-06 14:35:00 +0300

    rework debug provider. support editor provider

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/debugger/CSharpDebuggerProvider.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/debugger/CSharpDebuggerProvider.java
@@ -2,18 +2,34 @@ package org.mustbe.consulo.csharp.ide.debugger;
 
 import org.jetbrains.annotations.NotNull;
 import org.mustbe.consulo.csharp.lang.CSharpFileType;
+import org.mustbe.consulo.csharp.lang.psi.CSharpExpressionFragmentFactory;
 import org.mustbe.consulo.dotnet.debugger.DotNetDebuggerProvider;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 
 /**
  * @author VISTALL
  * @since 10.04.14
  */
-public class CSharpDebuggerProvider implements DotNetDebuggerProvider
+public class CSharpDebuggerProvider extends DotNetDebuggerProvider
 {
+	@NotNull
 	@Override
-	public boolean isSupported(@NotNull PsiFile psiFile)
+	public FileType getSupportedFileType()
 	{
-		return psiFile.getFileType() == CSharpFileType.INSTANCE;
+		return CSharpFileType.INSTANCE;
+	}
+
+	@NotNull
+	@Override
+	public PsiFile createExpressionCodeFragment(
+			@NotNull Project project,
+			@NotNull PsiElement sourcePosition,
+			@NotNull String text,
+			boolean isPhysical)
+	{
+		return CSharpExpressionFragmentFactory.createExpressionFragment(project, text);
 	}
 }
