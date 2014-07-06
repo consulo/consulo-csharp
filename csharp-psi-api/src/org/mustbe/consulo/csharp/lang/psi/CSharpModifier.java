3e776b371ba32d8276efb7a104661ad6cd51128e
VISTALL <vistall.valeriy@gmail.com>
2014-07-06 18:53:08 +0400

    moved shared modifiers

--- csharp-psi-api/src/org/mustbe/consulo/csharp/lang/psi/CSharpModifier.java
+++ csharp-psi-api/src/org/mustbe/consulo/csharp/lang/psi/CSharpModifier.java
@@ -16,6 +16,9 @@
 
 package org.mustbe.consulo.csharp.lang.psi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jetbrains.annotations.NotNull;
 import org.mustbe.consulo.dotnet.psi.DotNetModifier;
 
@@ -44,6 +47,18 @@ public enum CSharpModifier implements DotNetModifier
 	OUT;
 
 	public static CSharpModifier[] EMPTY_ARRAY = new CSharpModifier[0];
+	private static Map<DotNetModifier, CSharpModifier> ourReplaceMap = new HashMap<DotNetModifier, CSharpModifier>()
+	{
+		{
+			put(DotNetModifier.STATIC, CSharpModifier.STATIC);
+			put(DotNetModifier.PRIVATE, CSharpModifier.PRIVATE);
+			put(DotNetModifier.PUBLIC, CSharpModifier.PUBLIC);
+			put(DotNetModifier.PROTECTED, CSharpModifier.PROTECTED);
+			put(DotNetModifier.INTERNAL, CSharpModifier.INTERNAL);
+			put(DotNetModifier.ABSTRACT, CSharpModifier.ABSTRACT);
+			put(DotNetModifier.SEALED, CSharpModifier.SEALED);
+		}
+	};
 
 	private int myMask;
 
@@ -66,21 +81,17 @@ public enum CSharpModifier implements DotNetModifier
 	@NotNull
 	public static CSharpModifier as(DotNetModifier modifierWithMask)
 	{
-		if(modifierWithMask == DotNetModifier.STATIC)
-		{
-			return CSharpModifier.STATIC;
-		}
-		else if(modifierWithMask == DotNetModifier.SEALED)
-		{
-			return CSharpModifier.SEALED;
-		}
-
 		if(modifierWithMask instanceof CSharpModifier)
 		{
 			return (CSharpModifier) modifierWithMask;
 		}
 		else
 		{
+			CSharpModifier cSharpModifier = ourReplaceMap.get(modifierWithMask);
+			if(cSharpModifier != null)
+			{
+				return cSharpModifier;
+			}
 			throw new IllegalArgumentException(modifierWithMask + " is cant be casted to CSharpModifier");
 		}
 	}
