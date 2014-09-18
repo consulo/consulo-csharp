cb18c340b869832e181b75c09bfe91b328132fd5
VISTALL <vistall.valeriy@gmail.com>
2014-09-18 18:12:33 +0400

    CS0132

---
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/highlight/check/impl/CS0132.java
@@ -0,0 +1,26 @@
+package org.mustbe.consulo.csharp.ide.highlight.check.impl;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.csharp.ide.highlight.check.CompilerCheck;
+import org.mustbe.consulo.csharp.lang.psi.CSharpConstructorDeclaration;
+import org.mustbe.consulo.csharp.lang.psi.CSharpModifier;
+import org.mustbe.consulo.csharp.module.extension.CSharpLanguageVersion;
+
+/**
+ * @author VISTALL
+ * @since 18.09.14
+ */
+public class CS0132 extends CompilerCheck<CSharpConstructorDeclaration>
+{
+	@Nullable
+	@Override
+	public CompilerCheckResult checkImpl(@NotNull CSharpLanguageVersion languageVersion, @NotNull CSharpConstructorDeclaration element)
+	{
+		if(element.hasModifier(CSharpModifier.STATIC) && element.getParameters().length > 0)
+		{
+			return result(element.getParameterList(), formatElement(element));
+		}
+		return null;
+	}
+}
