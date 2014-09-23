7e5aeb6c0b52d864e7cc8b509b6a68c276e09974
VISTALL <vistall.valeriy@gmail.com>
2014-09-23 19:21:36 +0400

    set accessors return type is void

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/highlight/check/impl/CS0029.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/highlight/check/impl/CS0029.java
@@ -19,13 +19,13 @@ package org.mustbe.consulo.csharp.ide.highlight.check.impl;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mustbe.consulo.csharp.ide.highlight.check.CompilerCheck;
-import org.mustbe.consulo.csharp.lang.psi.CSharpArrayMethodDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpTokens;
 import org.mustbe.consulo.csharp.lang.psi.impl.CSharpTypeUtil;
 import org.mustbe.consulo.csharp.lang.psi.impl.msil.CSharpTransform;
 import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpAssignmentExpressionImpl;
 import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpOperatorReferenceImpl;
 import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpReturnStatementImpl;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpXXXAccessorImpl;
 import org.mustbe.consulo.csharp.module.extension.CSharpLanguageVersion;
 import org.mustbe.consulo.dotnet.DotNetTypes;
 import org.mustbe.consulo.dotnet.lang.psi.impl.source.resolve.type.DotNetTypeRefByQName;
@@ -34,7 +34,6 @@ import org.mustbe.consulo.dotnet.psi.DotNetExpression;
 import org.mustbe.consulo.dotnet.psi.DotNetMethodDeclaration;
 import org.mustbe.consulo.dotnet.psi.DotNetModifierListOwner;
 import org.mustbe.consulo.dotnet.psi.DotNetVariable;
-import org.mustbe.consulo.dotnet.psi.DotNetXXXAccessor;
 import org.mustbe.consulo.dotnet.resolve.DotNetTypeRef;
 import com.intellij.openapi.util.Trinity;
 import com.intellij.psi.PsiElement;
@@ -114,17 +113,9 @@ public class CS0029 extends CompilerCheck<PsiElement>
 			{
 				expected = ((DotNetMethodDeclaration) modifierListOwner).getReturnTypeRef();
 			}
-			else if(modifierListOwner instanceof DotNetXXXAccessor)
+			else if(modifierListOwner instanceof CSharpXXXAccessorImpl)
 			{
-				PsiElement parentOfAccessor = modifierListOwner.getParent();
-				if(parentOfAccessor instanceof DotNetVariable)
-				{
-					expected = ((DotNetVariable) parentOfAccessor).toTypeRef(false);
-				}
-				else if(parentOfAccessor instanceof CSharpArrayMethodDeclaration)
-				{
-					expected = ((CSharpArrayMethodDeclaration) parentOfAccessor).getReturnTypeRef();
-				}
+				expected = ((CSharpXXXAccessorImpl) modifierListOwner).getReturnTypeRef();
 			}
 
 			if(expected == null)
