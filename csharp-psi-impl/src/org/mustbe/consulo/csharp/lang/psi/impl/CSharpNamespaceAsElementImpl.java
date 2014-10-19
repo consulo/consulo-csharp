ba8ecdc71f5e0662c3cd12f03066159af6525bd0
VISTALL <vistall.valeriy@gmail.com>
2014-10-19 10:50:04 +0400

    compilation

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/CSharpNamespaceAsElementImpl.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/CSharpNamespaceAsElementImpl.java
@@ -5,16 +5,14 @@ import org.mustbe.consulo.csharp.lang.CSharpLanguage;
 import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpUsingListImpl;
 import org.mustbe.consulo.csharp.lang.psi.impl.source.resolve.util.CSharpResolveUtil;
 import org.mustbe.consulo.csharp.lang.psi.impl.stub.index.CSharpIndexKeys;
-import org.mustbe.consulo.csharp.lang.psi.impl.stub.index.MemberByAllNamespaceQNameIndex;
-import org.mustbe.consulo.csharp.lang.psi.impl.stub.index.MemberByNamespaceQNameIndex;
 import org.mustbe.consulo.dotnet.lang.psi.impl.IndexBasedDotNetNamespaceAsElement;
 import org.mustbe.consulo.dotnet.psi.DotNetNamespaceUtil;
+import org.mustbe.consulo.dotnet.resolve.impl.IndexBasedDotNetPsiSearcher;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.ResolveState;
 import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.stubs.StringStubIndexExtension;
 import com.intellij.psi.stubs.StubIndex;
 import com.intellij.util.Processor;
 
@@ -24,13 +22,18 @@ import com.intellij.util.Processor;
  */
 public class CSharpNamespaceAsElementImpl extends IndexBasedDotNetNamespaceAsElement
 {
-	public CSharpNamespaceAsElementImpl(@NotNull Project project, @NotNull String indexKey, @NotNull String qName)
+	public CSharpNamespaceAsElementImpl(@NotNull Project project,
+			@NotNull String indexKey,
+			@NotNull String qName,
+			@NotNull IndexBasedDotNetPsiSearcher searcher)
 	{
-		super(project, CSharpLanguage.INSTANCE, indexKey, qName);
+		super(project, CSharpLanguage.INSTANCE, indexKey, qName, searcher);
 	}
 
 	@Override
-	public boolean processDeclarations(@NotNull final PsiScopeProcessor processor, @NotNull final ResolveState state, final PsiElement lastParent,
+	public boolean processDeclarations(@NotNull final PsiScopeProcessor processor,
+			@NotNull final ResolveState state,
+			final PsiElement lastParent,
 			@NotNull final PsiElement place)
 	{
 		if(!super.processDeclarations(processor, state, lastParent, place))
@@ -44,8 +47,8 @@ public class CSharpNamespaceAsElementImpl extends IndexBasedDotNetNamespaceAsEle
 		}
 
 		GlobalSearchScope searchScope = state.get(RESOLVE_SCOPE);
-		return StubIndex.getInstance().processElements(CSharpIndexKeys.USING_LIST_INDEX, DotNetNamespaceUtil.getIndexableNamespace(myQName), myProject,
-				searchScope, CSharpUsingListImpl.class, new Processor<CSharpUsingListImpl>()
+		return StubIndex.getInstance().processElements(CSharpIndexKeys.USING_LIST_INDEX, DotNetNamespaceUtil.getIndexableNamespace(myQName),
+				myProject, searchScope, CSharpUsingListImpl.class, new Processor<CSharpUsingListImpl>()
 		{
 			@Override
 			public boolean process(CSharpUsingListImpl usingList)
@@ -53,19 +56,5 @@ public class CSharpNamespaceAsElementImpl extends IndexBasedDotNetNamespaceAsEle
 				return usingList.processDeclarations(processor, state, lastParent, place);
 			}
 		});
-	}
-
-	@NotNull
-	@Override
-	public StringStubIndexExtension<? extends PsiElement> getHardIndexExtension()
-	{
-		return MemberByNamespaceQNameIndex.getInstance();
-	}
-
-	@NotNull
-	@Override
-	public StringStubIndexExtension<? extends PsiElement> getSoftIndexExtension()
-	{
-		return MemberByAllNamespaceQNameIndex.getInstance();
 	}
 }
