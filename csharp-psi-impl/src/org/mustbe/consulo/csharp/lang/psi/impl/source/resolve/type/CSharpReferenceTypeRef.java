c379494614ff7fce52be2c03943facedfe0bfd34
VISTALL <vistall.valeriy@gmail.com>
2014-10-24 17:17:18 +0400

    delegates extends MulticastDelegate

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/type/CSharpReferenceTypeRef.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/type/CSharpReferenceTypeRef.java
@@ -5,7 +5,10 @@ import org.jetbrains.annotations.Nullable;
 import org.mustbe.consulo.csharp.lang.psi.CSharpMethodDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpReferenceExpression;
 import org.mustbe.consulo.csharp.lang.psi.impl.CSharpTypeUtil;
+import org.mustbe.consulo.csharp.lang.psi.impl.msil.CSharpTransform;
+import org.mustbe.consulo.dotnet.DotNetTypes;
 import org.mustbe.consulo.dotnet.resolve.DotNetGenericExtractor;
+import org.mustbe.consulo.dotnet.resolve.DotNetPsiSearcher;
 import org.mustbe.consulo.dotnet.resolve.DotNetTypeRef;
 import org.mustbe.consulo.dotnet.resolve.DotNetTypeResolveResult;
 import com.intellij.psi.PsiElement;
@@ -49,9 +52,20 @@ public class CSharpReferenceTypeRef implements DotNetTypeRef
 
 	public static class LambdaResult extends Result implements CSharpLambdaResolveResult
 	{
-		public LambdaResult(PsiElement element)
+		private final PsiElement myScope;
+
+		public LambdaResult(PsiElement scope, PsiElement element)
 		{
 			super(element);
+			myScope = scope;
+		}
+
+		@Nullable
+		@Override
+		public PsiElement getElement()
+		{
+			return DotNetPsiSearcher.getInstance(myScope.getProject()).findType(DotNetTypes.System.MulticastDelegate,
+					myScope.getResolveScope(), DotNetPsiSearcher.TypeResoleKind.UNKNOWN, CSharpTransform.INSTANCE);
 		}
 
 		@NotNull
@@ -99,12 +113,12 @@ public class CSharpReferenceTypeRef implements DotNetTypeRef
 
 	@NotNull
 	@Override
-	public DotNetTypeResolveResult resolve(@NotNull PsiElement element)
+	public DotNetTypeResolveResult resolve(@NotNull PsiElement scope)
 	{
 		PsiElement resolve = myReferenceExpression.resolve();
 		if(resolve instanceof CSharpMethodDeclaration && ((CSharpMethodDeclaration) resolve).isDelegate())
 		{
-			return new LambdaResult(resolve);
+			return new LambdaResult(scope, resolve);
 		}
 		return new Result(resolve);
 	}
