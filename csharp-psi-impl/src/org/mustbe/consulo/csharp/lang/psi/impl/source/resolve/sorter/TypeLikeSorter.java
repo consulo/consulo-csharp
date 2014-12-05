fc236781efd11e8fffd2ec922f53f9525b2cac34
VISTALL <vistall.valeriy@gmail.com>
2014-12-05 21:58:14 +0300

    inherit extractor from reference

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/sorter/TypeLikeSorter.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/sorter/TypeLikeSorter.java
@@ -4,11 +4,16 @@ import java.util.Comparator;
 
 import org.jetbrains.annotations.NotNull;
 import org.mustbe.consulo.csharp.lang.psi.CSharpReferenceExpression;
-import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpReferenceExpressionImplUtil;
+import org.mustbe.consulo.csharp.lang.psi.CSharpTypeDeclaration;
+import org.mustbe.consulo.csharp.lang.psi.impl.light.CSharpLightTypeDeclaration;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.resolve.type.CSharpGenericExtractor;
+import org.mustbe.consulo.dotnet.psi.DotNetGenericParameter;
 import org.mustbe.consulo.dotnet.psi.DotNetGenericParameterListOwner;
 import org.mustbe.consulo.dotnet.psi.DotNetVariable;
 import org.mustbe.consulo.dotnet.resolve.DotNetNamespaceAsElement;
+import org.mustbe.consulo.dotnet.resolve.DotNetTypeRef;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementResolveResult;
 import com.intellij.psi.ResolveResult;
 import com.intellij.util.containers.ContainerUtil;
 
@@ -62,24 +67,46 @@ public class TypeLikeSorter implements ResolveResultSorter
 	@NotNull
 	public static TypeLikeSorter createByReference(@NotNull PsiElement element)
 	{
-		int size = 0;
+		DotNetTypeRef[] typeRefs = DotNetTypeRef.EMPTY_ARRAY;
 		if(element instanceof CSharpReferenceExpression)
 		{
-			size = CSharpReferenceExpressionImplUtil.getTypeArgumentListSize((CSharpReferenceExpression) element);
+			typeRefs = ((CSharpReferenceExpression) element).getTypeArgumentListRefs();
 		}
-		return new TypeLikeSorter(size);
+		return new TypeLikeSorter(typeRefs);
 	}
 
 	private final ResolveResultComparator myComparator;
 
-	public TypeLikeSorter(int genericCount)
+	private final DotNetTypeRef[] myTypeArgumentRefs;
+
+	public TypeLikeSorter(DotNetTypeRef[] typeRefs)
 	{
-		myComparator = new ResolveResultComparator(genericCount);
+		myTypeArgumentRefs = typeRefs;
+		myComparator = new ResolveResultComparator(myTypeArgumentRefs.length);
 	}
 
 	@Override
 	public void sort(@NotNull ResolveResult[] resolveResults)
 	{
 		ContainerUtil.sort(resolveResults, myComparator);
+
+		if(myTypeArgumentRefs.length > 0)
+		{
+			for(int i = 0; i < resolveResults.length; i++)
+			{
+				ResolveResult resolveResult = resolveResults[i];
+				PsiElement element = resolveResult.getElement();
+				if(element instanceof CSharpTypeDeclaration)
+				{
+					CSharpTypeDeclaration typeDeclaration = (CSharpTypeDeclaration) element;
+					DotNetGenericParameter[] genericParameters = typeDeclaration.getGenericParameters();
+					if(genericParameters.length == myTypeArgumentRefs.length)
+					{
+						resolveResults[i] = new PsiElementResolveResult(new CSharpLightTypeDeclaration(typeDeclaration,
+								new CSharpGenericExtractor(genericParameters, myTypeArgumentRefs)));
+					}
+				}
+			}
+		}
 	}
 }
