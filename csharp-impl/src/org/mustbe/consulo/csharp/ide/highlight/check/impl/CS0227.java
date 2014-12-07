ec410226e5439b87fc94eea3178755bcd1d31c8d
VISTALL <vistall.valeriy@gmail.com>
2014-12-07 13:33:15 +0300

    quickfixes for CS0027

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/highlight/check/impl/CS0227.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/highlight/check/impl/CS0227.java
@@ -17,38 +17,113 @@
 package org.mustbe.consulo.csharp.ide.highlight.check.impl;
 
 import org.jetbrains.annotations.NotNull;
-import org.mustbe.consulo.csharp.ide.highlight.check.AbstractCompilerCheck;
+import org.jetbrains.annotations.Nullable;
+import org.mustbe.consulo.csharp.ide.codeInsight.actions.RemoveModifierFix;
+import org.mustbe.consulo.csharp.ide.highlight.check.CompilerCheck;
 import org.mustbe.consulo.csharp.lang.psi.CSharpModifier;
 import org.mustbe.consulo.csharp.lang.psi.CSharpModifierList;
 import org.mustbe.consulo.csharp.module.extension.BaseCSharpModuleExtension;
+import org.mustbe.consulo.csharp.module.extension.CSharpLanguageVersion;
+import org.mustbe.consulo.dotnet.psi.DotNetModifierListOwner;
+import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.SmartPointerManager;
+import com.intellij.psi.SmartPsiElementPointer;
+import com.intellij.util.IncorrectOperationException;
+import lombok.val;
 
 /**
  * @author VISTALL
  * @since 15.05.14
  */
-public class CS0227 extends AbstractCompilerCheck<CSharpModifierList>
+public class CS0227 extends CompilerCheck<CSharpModifierList>
 {
-	@Override
-	public boolean accept(@NotNull CSharpModifierList list)
+	public static class AllowUnsafeCodeFix extends BaseIntentionAction
 	{
-		PsiElement modifier = list.getModifierElement(CSharpModifier.UNSAFE);
-		if(modifier == null)
+		private SmartPsiElementPointer<PsiElement> myPointer;
+
+		public AllowUnsafeCodeFix(PsiElement element)
 		{
-			return false;
+			myPointer = SmartPointerManager.getInstance(element.getProject()).createSmartPsiElementPointer(element);
 		}
 
-		BaseCSharpModuleExtension extension = ModuleUtilCore.getExtension(list, BaseCSharpModuleExtension.class);
+		@NotNull
+		@Override
+		public String getText()
+		{
+			return "Allow unsafe code";
+		}
 
-		return extension != null && !extension.isAllowUnsafeCode();
+		@NotNull
+		@Override
+		public String getFamilyName()
+		{
+			return "C#";
+		}
+
+		@Override
+		public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file)
+		{
+			return myPointer.getElement() != null;
+		}
+
+		@Override
+		public boolean startInWriteAction()
+		{
+			return true;
+		}
+
+		@Override
+		public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException
+		{
+			PsiElement element = myPointer.getElement();
+			if(element == null)
+			{
+				return;
+			}
+
+			Module moduleForPsiElement = ModuleUtilCore.findModuleForPsiElement(element);
+			if(moduleForPsiElement == null)
+			{
+				return;
+			}
+
+			ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(moduleForPsiElement);
+
+			val modifiableModel = moduleRootManager.getModifiableModel();
+			BaseCSharpModuleExtension cSharpModuleExtension = modifiableModel.getExtension(BaseCSharpModuleExtension.class);
+			if(cSharpModuleExtension != null)
+			{
+				cSharpModuleExtension.setAllowUnsafeCode(true);
+			}
+
+			modifiableModel.commit();
+		}
 	}
 
+	@Nullable
 	@Override
-	public void checkImpl(@NotNull CSharpModifierList list, @NotNull CompilerCheckBuilder checkResult)
+	public HighlightInfoFactory checkImpl(@NotNull CSharpLanguageVersion languageVersion, @NotNull CSharpModifierList element)
 	{
-		PsiElement modifier = list.getModifierElement(CSharpModifier.UNSAFE);
-		assert modifier != null;
-		checkResult.setTextRange(modifier.getTextRange());
+		PsiElement modifier = element.getModifierElement(CSharpModifier.UNSAFE);
+		if(modifier == null)
+		{
+			return null;
+		}
+
+		BaseCSharpModuleExtension extension = ModuleUtilCore.getExtension(element, BaseCSharpModuleExtension.class);
+
+		if(extension != null && !extension.isAllowUnsafeCode())
+		{
+			return newBuilder(modifier).addQuickFix(new AllowUnsafeCodeFix(element)).addQuickFix(new RemoveModifierFix(CSharpModifier.UNSAFE,
+					(DotNetModifierListOwner) element.getParent()));
+		}
+		return null;
 	}
 }
