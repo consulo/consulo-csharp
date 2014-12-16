744ce966c2f972ae2811ff865ecaa53107ea680a
VISTALL <vistall.valeriy@gmail.com>
2014-12-16 11:30:22 +0300

    fixed #193

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/type/CSharpConstantTypeRef.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/type/CSharpConstantTypeRef.java
@@ -17,7 +17,9 @@
 package org.mustbe.consulo.csharp.lang.psi.impl.source.resolve.type;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.mustbe.consulo.csharp.lang.psi.impl.CSharpTypeUtil;
+import org.mustbe.consulo.dotnet.DotNetTypes;
 import org.mustbe.consulo.dotnet.resolve.DotNetTypeRef;
 import com.intellij.psi.PsiElement;
 
@@ -27,9 +29,18 @@ import com.intellij.psi.PsiElement;
  */
 public class CSharpConstantTypeRef extends DotNetTypeRef.Delegate implements CSharpChameleonTypeRef
 {
-	public CSharpConstantTypeRef(DotNetTypeRef defaultTypeRef)
+	// this value always cached on JVM ?
+	private static final Integer ZERO = 0;
+
+	private static final DotNetTypeRef ourEnumTypeRef = new CSharpTypeRefByQName(DotNetTypes.System.Enum);
+
+	@Nullable
+	private final Object myValue;
+
+	public CSharpConstantTypeRef(@NotNull DotNetTypeRef defaultTypeRef, @Nullable Object value)
 	{
 		super(defaultTypeRef);
+		myValue = value;
 	}
 
 	@NotNull
@@ -39,6 +50,11 @@ public class CSharpConstantTypeRef extends DotNetTypeRef.Delegate implements CSh
 		int topRank = CSharpTypeUtil.getNumberRank(getDelegate(), scope);
 		int targetRank = CSharpTypeUtil.getNumberRank(another, scope);
 		if(targetRank != -1 && targetRank < topRank)
+		{
+			return another;
+		}
+
+		if(myValue == ZERO && CSharpTypeUtil.isInheritable(ourEnumTypeRef, another, scope))
 		{
 			return another;
 		}
