a279ff8c13c293b5596b81469e2515f647a4bbe8
VISTALL <vistall.valeriy@gmail.com>
2014-12-24 19:53:17 +0300

    dont broke psi tree if we typing insde parameter list

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/parser/decl/MethodParsing.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/parser/decl/MethodParsing.java
@@ -24,6 +24,7 @@ import org.mustbe.consulo.csharp.lang.parser.stmt.StatementParsing;
 import org.mustbe.consulo.csharp.lang.psi.CSharpElements;
 import org.mustbe.consulo.csharp.lang.psi.CSharpStubElements;
 import com.intellij.lang.PsiBuilder;
+import com.intellij.openapi.util.Pair;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.util.BitUtil;
 import lombok.val;
@@ -56,8 +57,10 @@ public class MethodParsing extends MemberWithBodyParsing
 		}
 	}
 
-	public static void parseMethodStartAfterType(@NotNull CSharpBuilderWrapper builder, @NotNull PsiBuilder.Marker marker,
-			@Nullable TypeInfo typeInfo, @NotNull Target target)
+	public static void parseMethodStartAfterType(@NotNull CSharpBuilderWrapper builder,
+			@NotNull PsiBuilder.Marker marker,
+			@Nullable TypeInfo typeInfo,
+			@NotNull Target target)
 	{
 		if(target == Target.CONSTRUCTOR || target == Target.DECONSTRUCTOR)
 		{
@@ -182,15 +185,21 @@ public class MethodParsing extends MemberWithBodyParsing
 		{
 			while(!builder.eof())
 			{
-				parseParameter(builder, flags);
+				parseParameter(builder, end, flags);
 
 				if(builder.getTokenType() == COMMA)
 				{
 					builder.advanceLexer();
 				}
-				else
+				else if(builder.getTokenType() == end)
 				{
 					break;
+				}
+				else
+				{
+					PsiBuilder.Marker errorMarker = builder.mark();
+					builder.advanceLexer();
+					errorMarker.error("Expected comma");
 				}
 			}
 		}
@@ -199,14 +208,23 @@ public class MethodParsing extends MemberWithBodyParsing
 		mark.done(BitUtil.isSet(flags, STUB_SUPPORT) ? CSharpStubElements.PARAMETER_LIST : CSharpElements.PARAMETER_LIST);
 	}
 
-	private static void parseParameter(CSharpBuilderWrapper builder, int flags)
+	private static void parseParameter(CSharpBuilderWrapper builder, IElementType end, int flags)
 	{
 		val mark = builder.mark();
 
-		parseModifierListWithAttributes(builder, flags);
+		Pair<PsiBuilder.Marker, Boolean> modifierPair = parseModifierListWithAttributes(builder, flags);
 
 		if(parseType(builder, flags) == null)
 		{
+			if(modifierPair.getSecond() == Boolean.TRUE)
+			{
+				// if no modifiers but we failed parse type - need go advance, due ill infinity loop inside parseParameterList,
+				// but dont eat close brace
+				if(builder.getTokenType() != end)
+				{
+					builder.advanceLexer();
+				}
+			}
 			mark.error("Type expected");
 		}
 		else
