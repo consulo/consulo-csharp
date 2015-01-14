282592f78b150f88ca7b54be319965094e3cae18
VISTALL <vistall.valeriy@gmail.com>
2015-01-14 03:44:23 +0300

    fixed #222

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/type/CSharpModuleTypeHelperImpl.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/source/resolve/type/CSharpModuleTypeHelperImpl.java
@@ -70,7 +70,19 @@ public class CSharpModuleTypeHelperImpl extends CSharpModuleTypeHelper
 			builder.append("public class ArrayImpl<T> : System.Array, System.Collections.Generic.IEnumerable<T>");
 			builder.append("{");
 			builder.append("private ArrayImpl() {}");
+			builder.append("public T this[long index");
+			for(int i = 0; i < dimensions; i++)
+			{
+				builder.append(", int index").append(i);
+			}
+			builder.append("] { get; set; }");
 			builder.append("public T this[int index");
+			for(int i = 0; i < dimensions; i++)
+			{
+				builder.append(", int index").append(i);
+			}
+			builder.append("] { get; set; }");
+			builder.append("public T this[uint index");
 			for(int i = 0; i < dimensions; i++)
 			{
 				builder.append(", int index").append(i);
