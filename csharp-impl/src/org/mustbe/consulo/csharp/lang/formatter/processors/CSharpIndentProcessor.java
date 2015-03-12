ef6bd61904c9566b8a5a243c2f10bba3a1eb5e50
VISTALL <vistall.valeriy@gmail.com>
2015-03-12 05:03:43 +0300

    fixed indent for docs

--- csharp-impl/src/org/mustbe/consulo/csharp/lang/formatter/processors/CSharpIndentProcessor.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/lang/formatter/processors/CSharpIndentProcessor.java
@@ -1,11 +1,7 @@
 package org.mustbe.consulo.csharp.lang.formatter.processors;
 
 import org.mustbe.consulo.csharp.ide.codeStyle.CSharpCodeStyleSettings;
-import org.mustbe.consulo.csharp.lang.doc.psi.CSharpDocRoot;
-import org.mustbe.consulo.csharp.lang.doc.psi.CSharpDocText;
-import org.mustbe.consulo.csharp.lang.doc.psi.CSharpDocTokenType;
 import org.mustbe.consulo.csharp.lang.psi.CSharpElements;
-import org.mustbe.consulo.csharp.lang.psi.CSharpFile;
 import org.mustbe.consulo.csharp.lang.psi.CSharpStatementAsStatementOwner;
 import org.mustbe.consulo.csharp.lang.psi.CSharpStubElements;
 import org.mustbe.consulo.csharp.lang.psi.CSharpTokens;
@@ -19,7 +15,6 @@ import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.codeInsight.CommentUtilCore;
 import lombok.val;
 
@@ -62,27 +57,6 @@ public class CSharpIndentProcessor implements CSharpTokens, CSharpElements
 				elementType == USING_LIST ||
 				elementType == CONSTRUCTOR_DECLARATION)
 		{
-			return Indent.getNormalIndent();
-		}
-		else if(elementType == CSharpDocTokenType.DOC_LINE_START)
-		{
-			if(parent != null && parent.getParent() instanceof PsiFile)
-			{
-				return Indent.getNoneIndent();
-			}
-			if(parent instanceof CSharpDocText)
-			{
-				return Indent.getNoneIndent();
-			}
-			return Indent.getNormalIndent();
-		}
-		else if(psi instanceof CSharpDocText)
-		{
-			CSharpDocRoot docRoot = PsiTreeUtil.getParentOfType(psi, CSharpDocRoot.class);
-			if(docRoot != null && docRoot.getParent() instanceof CSharpFile)
-			{
-				return Indent.getNoneIndent();
-			}
 			return Indent.getNormalIndent();
 		}
 		else if(parent instanceof CSharpArrayInitializerSingleValueImpl)
