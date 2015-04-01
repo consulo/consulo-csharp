daea8b3d09a84051a8a04eb3d7e72304d00bb771
VISTALL <vistall.valeriy@gmail.com>
2015-04-01 17:14:27 +0300

    disable search for parameter usage

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/codeInspection/unusedSymbol/UnusedSymbolVisitor.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/codeInspection/unusedSymbol/UnusedSymbolVisitor.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import org.mustbe.consulo.csharp.lang.psi.CSharpElementVisitor;
 import org.mustbe.consulo.csharp.lang.psi.CSharpLocalVariable;
-import org.mustbe.consulo.dotnet.psi.DotNetParameter;
 import com.intellij.psi.PsiNameIdentifierOwner;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.searches.ReferencesSearch;
@@ -24,24 +23,9 @@ public class UnusedSymbolVisitor extends CSharpElementVisitor
 	@Override
 	public void visitLocalVariable(CSharpLocalVariable variable)
 	{
-		Query<PsiReference> query = ReferencesSearch.search(variable);
+		Query<PsiReference> query = ReferencesSearch.search(variable, variable.getUseScope());
 
 		myVariableStates.put(variable, query.findFirst() != null);
-	}
-
-	@Override
-	public void visitParameter(DotNetParameter parameter)
-	{
-		for(UnusedElementPolicy unusedElementPolicy : UnusedElementPolicy.EP_NAME.getExtensions())
-		{
-			if(!unusedElementPolicy.canMarkAsUnused(parameter))
-			{
-				return;
-			}
-		}
-		Query<PsiReference> query = ReferencesSearch.search(parameter);
-
-		myVariableStates.put(parameter, query.findFirst() != null);
 	}
 
 	@NotNull
