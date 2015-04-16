d3a195176b8441674d9807f55337ee36aa4cb31b
VISTALL <vistall.valeriy@gmail.com>
2015-04-16 13:47:01 +0300

    more filters

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/findUsage/usageType/CSharpUsageTypeProvider.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/findUsage/usageType/CSharpUsageTypeProvider.java
@@ -1,9 +1,13 @@
 package org.mustbe.consulo.csharp.ide.findUsage.usageType;
 
 import org.jetbrains.annotations.Nullable;
-import org.mustbe.consulo.csharp.ide.highlight.CSharpHighlightUtil;
+import org.mustbe.consulo.csharp.lang.psi.CSharpFieldDeclaration;
+import org.mustbe.consulo.csharp.lang.psi.CSharpLocalVariable;
+import org.mustbe.consulo.csharp.lang.psi.CSharpMethodDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpReferenceExpression;
+import org.mustbe.consulo.dotnet.psi.DotNetType;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.usages.impl.rules.UsageType;
 import com.intellij.usages.impl.rules.UsageTypeProvider;
 
@@ -20,15 +24,43 @@ public class CSharpUsageTypeProvider implements UsageTypeProvider
 	@Override
 	public UsageType getUsageType(PsiElement element)
 	{
-		if(CSharpHighlightUtil.isMethodRef(element, null))
+		if(element instanceof CSharpReferenceExpression)
 		{
-			return AS_METHOD_REF;
-		}
-
-		if(element instanceof CSharpReferenceExpression && ((CSharpReferenceExpression) element).kind() == CSharpReferenceExpression.ResolveToKind
-				.METHOD)
-		{
-			return METHOD_CALL;
+			PsiElement resolve = ((CSharpReferenceExpression) element).resolve();
+			if(resolve == null)
+			{
+				return null;
+			}
+			CSharpReferenceExpression.ResolveToKind kind = ((CSharpReferenceExpression) element).kind();
+			switch(kind)
+			{
+				case METHOD:
+					return METHOD_CALL;
+				case CONSTRUCTOR:
+					return UsageType.CLASS_NEW_OPERATOR;
+				case TYPE_LIKE:
+					DotNetType type = PsiTreeUtil.getParentOfType(element, DotNetType.class);
+					if(type == null)
+					{
+						return null;
+					}
+					PsiElement parent = type.getParent();
+					if(parent instanceof CSharpLocalVariable)
+					{
+						return UsageType.CLASS_LOCAL_VAR_DECLARATION;
+					}
+					else if(parent instanceof CSharpFieldDeclaration)
+					{
+						return UsageType.CLASS_FIELD_DECLARATION;
+					}
+					break;
+				case ANY_MEMBER:
+					if(element instanceof CSharpMethodDeclaration && !((CSharpMethodDeclaration) element).isDelegate())
+					{
+						return AS_METHOD_REF;
+					}
+					break;
+			}
 		}
 		return null;
 	}
