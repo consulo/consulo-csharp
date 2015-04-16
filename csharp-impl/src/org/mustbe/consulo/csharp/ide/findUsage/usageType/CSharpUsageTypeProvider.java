115e6325796c61137af2da80f1c575b813f1f1c6
VISTALL <vistall.valeriy@gmail.com>
2015-04-16 14:02:03 +0300

    ore usage filters

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/findUsage/usageType/CSharpUsageTypeProvider.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/findUsage/usageType/CSharpUsageTypeProvider.java
@@ -5,6 +5,12 @@ import org.mustbe.consulo.csharp.lang.psi.CSharpFieldDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpLocalVariable;
 import org.mustbe.consulo.csharp.lang.psi.CSharpMethodDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpReferenceExpression;
+import org.mustbe.consulo.csharp.lang.psi.CSharpSimpleLikeMethodAsElement;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpAsExpressionImpl;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpIsExpressionImpl;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpTypeCastExpressionImpl;
+import org.mustbe.consulo.dotnet.psi.DotNetAttribute;
+import org.mustbe.consulo.dotnet.psi.DotNetParameter;
 import org.mustbe.consulo.dotnet.psi.DotNetType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -19,6 +25,9 @@ public class CSharpUsageTypeProvider implements UsageTypeProvider
 {
 	public static final UsageType AS_METHOD_REF = new UsageType("As method reference");
 	public static final UsageType METHOD_CALL = new UsageType("Method call");
+	public static final UsageType ATTRIBUTE = new UsageType("Attribute");
+	public static final UsageType CLASS_IN_AS = new UsageType("Usage in 'as'");
+	public static final UsageType CLASS_IN_IS = new UsageType("Usage in 'is'");
 
 	@Nullable
 	@Override
@@ -37,6 +46,10 @@ public class CSharpUsageTypeProvider implements UsageTypeProvider
 				case METHOD:
 					return METHOD_CALL;
 				case CONSTRUCTOR:
+					if(element.getParent() instanceof DotNetAttribute)
+					{
+						return ATTRIBUTE;
+					}
 					return UsageType.CLASS_NEW_OPERATOR;
 				case TYPE_LIKE:
 					DotNetType type = PsiTreeUtil.getParentOfType(element, DotNetType.class);
@@ -52,6 +65,26 @@ public class CSharpUsageTypeProvider implements UsageTypeProvider
 					else if(parent instanceof CSharpFieldDeclaration)
 					{
 						return UsageType.CLASS_FIELD_DECLARATION;
+					}
+					else if(parent instanceof DotNetParameter)
+					{
+						return UsageType.CLASS_METHOD_PARAMETER_DECLARATION;
+					}
+					else if(parent instanceof CSharpSimpleLikeMethodAsElement)
+					{
+						return UsageType.CLASS_METHOD_RETURN_TYPE;
+					}
+					else if(parent instanceof CSharpTypeCastExpressionImpl)
+					{
+						return UsageType.CLASS_CAST_TO;
+					}
+					else if(parent instanceof CSharpAsExpressionImpl)
+					{
+						return CLASS_IN_AS;
+					}
+					else if(parent instanceof CSharpIsExpressionImpl)
+					{
+						return CLASS_IN_IS;
 					}
 					break;
 				case ANY_MEMBER:
