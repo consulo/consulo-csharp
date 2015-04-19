dcb7190294b8e80f0a8e5509fbe2b2d1447bcc0a
VISTALL <vistall.valeriy@gmail.com>
2015-04-19 04:18:46 +0300

    formattings tests

---
+++ csharp-impl/testData/formatting/Issue270.before.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int b = 1;
+
+        if (b > 0)
+            Console.WriteLine("test");
+        else if (b < 0)
+            Console.WriteLine("test");
+        else if (b < 0)
+                Console.WriteLine("test");
+            else if (b < 0)
+                    Console.WriteLine("test");
+    }
+}
---
+++ csharp-impl/testSrc/org/musbe/consulo/csharp/parsing/CSharpFormattingTest.java
@@ -0,0 +1,31 @@
+package org.musbe.consulo.csharp.parsing;
+
+import org.mustbe.consulo.testFramework.FormattingTestCase;
+
+/**
+ * @author VISTALL
+ * @since 19.04.2015
+ */
+public class CSharpFormattingTest extends FormattingTestCase
+{
+	public CSharpFormattingTest()
+	{
+		super("formatting", "cs");
+	}
+
+	@Override
+	protected boolean shouldContainTempFiles()
+	{
+		return false;
+	}
+
+	@Override
+	protected String getTestDataPath()
+	{
+		return "/csharp-impl/testData";
+	}
+
+	public void testIssue270()
+	{
+	}
+}
