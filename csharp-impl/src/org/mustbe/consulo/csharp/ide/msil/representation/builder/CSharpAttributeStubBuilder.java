58f716ee0f9ea3326bcc226b300af32907f0d08c
VISTALL <vistall.valeriy@gmail.com>
2015-05-26 18:38:07 +0300

    build stubs for named fields(and properties) for attributes

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/msil/representation/builder/CSharpAttributeStubBuilder.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/msil/representation/builder/CSharpAttributeStubBuilder.java
@@ -1,6 +1,10 @@
 package org.mustbe.consulo.csharp.ide.msil.representation.builder;
 
+import org.consulo.lombok.annotations.Logger;
+import org.jetbrains.annotations.NotNull;
 import org.mustbe.consulo.RequiredReadAction;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.resolve.type.CSharpArrayTypeRef;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.resolve.type.CSharpTypeRefByQName;
 import org.mustbe.consulo.dotnet.DotNetTypes;
 import org.mustbe.consulo.dotnet.resolve.DotNetTypeRef;
 import org.mustbe.consulo.msil.lang.psi.MsilCustomAttribute;
@@ -10,11 +14,17 @@ import org.mustbe.consulo.msil.lang.psi.impl.type.MsilReferenceTypeRefImpl;
 import org.mustbe.dotnet.msil.decompiler.textBuilder.util.XStubUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import edu.arizona.cs.mbel.io.ByteBuffer;
+import edu.arizona.cs.mbel.signature.ArrayTypeSignature;
+import edu.arizona.cs.mbel.signature.ClassTypeSignature;
+import edu.arizona.cs.mbel.signature.TypeSignature;
+import edu.arizona.cs.mbel.signature.TypeSignatureParser;
+import edu.arizona.cs.mbel.signature.ValueTypeSignature;
 
 /**
  * @author VISTALL
  * @since 21.03.14
  */
+@Logger
 public class CSharpAttributeStubBuilder
 {
 	@RequiredReadAction
@@ -50,6 +60,23 @@ public class CSharpAttributeStubBuilder
 			}
 		}
 
+		if(innerValue != null && byteBuffer.canRead())
+		{
+			int count = byteBuffer.getShort();
+			for(int i = 0; i < count; i++)
+			{
+				innerValue.append(", ");
+
+				byteBuffer.get(); //type  0x53 field 0x54 property
+				TypeSignature typeSignature = TypeSignatureParser.parse(byteBuffer, null);
+				String name = XStubUtil.getUtf8(byteBuffer);
+
+				innerValue.append(name).append(" = ");
+
+				appendValue(innerValue, toTypeRef(typeSignature), byteBuffer);
+			}
+		}
+
 		if(StringUtil.isEmpty(innerValue))
 		{
 			return;
@@ -60,13 +87,87 @@ public class CSharpAttributeStubBuilder
 		builder.append(")");
 	}
 
+	@NotNull
+	private static DotNetTypeRef toTypeRef(TypeSignature typeSignature)
+	{
+		if(typeSignature == TypeSignature.BOOLEAN)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Boolean);
+		}
+		else if(typeSignature == TypeSignature.STRING)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.String);
+		}
+		else if(typeSignature == TypeSignature.U1)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Byte);
+		}
+		else if(typeSignature == TypeSignature.I1)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.SByte);
+		}
+		if(typeSignature == TypeSignature.U2)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.UInt16);
+		}
+		else if(typeSignature == TypeSignature.I2)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Int16);
+		}
+		else if(typeSignature == TypeSignature.U4)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.UInt32);
+		}
+		else if(typeSignature == TypeSignature.I4)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Int32);
+		}
+		else if(typeSignature == TypeSignature.U8)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.UInt64);
+		}
+		else if(typeSignature == TypeSignature.I8)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Int64);
+		}
+		else if(typeSignature == TypeSignature.R4)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Single);
+		}
+		else if(typeSignature == TypeSignature.R8)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Double);
+		}
+		else if(typeSignature == TypeSignature.CHAR)
+		{
+			return new CSharpTypeRefByQName(DotNetTypes.System.Char);
+		}
+		else if(typeSignature instanceof ArrayTypeSignature)
+		{
+			return new CSharpArrayTypeRef(toTypeRef(((ArrayTypeSignature) typeSignature).getElementType()), 0);
+		}
+		else if(typeSignature instanceof ValueTypeSignature)
+		{
+			return new CSharpTypeRefByQName(((ValueTypeSignature) typeSignature).getValueType().getFullName());
+		}
+		else if(typeSignature instanceof ClassTypeSignature)
+		{
+			return new CSharpTypeRefByQName(((ClassTypeSignature) typeSignature).getClassType().getFullName());
+		}
+		else
+		{
+			LOGGER.error("Unknown how convert: " + typeSignature.toString() + ":0x" + Integer.toHexString(typeSignature.getType()));
+			return new CSharpTypeRefByQName(DotNetTypes.System.Object);
+		}
+	}
+
 	private static void appendValue(StringBuilder builder, DotNetTypeRef parameterTypeRef, ByteBuffer byteBuffer)
 	{
 		if(parameterTypeRef instanceof MsilArrayTypRefImpl)
 		{
 			builder.append("arrayError");
 		}
-		else if(parameterTypeRef instanceof MsilReferenceTypeRefImpl)
+		else if(parameterTypeRef instanceof MsilReferenceTypeRefImpl || parameterTypeRef instanceof CSharpTypeRefByQName)
 		{
 			String qualifiedText = parameterTypeRef.getQualifiedText();
 			if(qualifiedText.equals(DotNetTypes.System.Int32))
