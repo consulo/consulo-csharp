1d2012e803de2f9f5a29e314c7a8c2d60036ec0f
VISTALL <vistall.valeriy@gmail.com>
2015-08-07 19:18:53 +0300

    fixed keyword completion with qualified reference

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/completion/patterns/CSharpPatterns.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/completion/patterns/CSharpPatterns.java
@@ -19,8 +19,11 @@ package org.mustbe.consulo.csharp.ide.completion.patterns;
 import org.jetbrains.annotations.NotNull;
 import org.mustbe.consulo.RequiredReadAction;
 import org.mustbe.consulo.csharp.lang.psi.CSharpLocalVariable;
+import org.mustbe.consulo.csharp.lang.psi.CSharpReferenceExpression;
 import org.mustbe.consulo.csharp.lang.psi.CSharpTokens;
+import org.mustbe.consulo.csharp.lang.psi.CSharpUserType;
 import org.mustbe.consulo.csharp.lang.psi.impl.source.CSharpPsiUtilImpl;
+import org.mustbe.consulo.dotnet.psi.DotNetType;
 import com.intellij.patterns.PatternCondition;
 import com.intellij.patterns.PsiElementPattern;
 import com.intellij.patterns.StandardPatterns;
@@ -46,7 +49,21 @@ public class CSharpPatterns
 			public boolean accepts(@NotNull PsiElement element, ProcessingContext context)
 			{
 				CSharpLocalVariable localVariable = PsiTreeUtil.getParentOfType(element, CSharpLocalVariable.class);
-				return localVariable != null && CSharpPsiUtilImpl.isNullOrEmpty(localVariable);
+				if(localVariable == null)
+				{
+					return false;
+				}
+				DotNetType type = localVariable.getType();
+				if(!(type instanceof CSharpUserType))
+				{
+					return false;
+				}
+				CSharpReferenceExpression referenceExpression = ((CSharpUserType) type).getReferenceExpression();
+				if(referenceExpression.getQualifier() != null)
+				{
+					return false;
+				}
+				return CSharpPsiUtilImpl.isNullOrEmpty(localVariable);
 			}
 		});
 	}
