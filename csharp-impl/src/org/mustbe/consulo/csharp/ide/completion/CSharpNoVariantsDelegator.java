42463438e19104d53b99ecd0b02491852f34c86c
VISTALL <vistall.valeriy@gmail.com>
2015-08-23 16:10:44 +0300

    remove dead class

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/completion/CSharpNoVariantsDelegator.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/completion/CSharpNoVariantsDelegator.java
@@ -1,18 +1,47 @@
 package org.mustbe.consulo.csharp.ide.completion;
 
+import java.util.Collections;
+
 import org.mustbe.consulo.RequiredReadAction;
+import org.mustbe.consulo.csharp.ide.codeInsight.actions.AddUsingAction;
+import org.mustbe.consulo.csharp.ide.completion.item.ReplaceableTypeLikeLookupElement;
+import org.mustbe.consulo.csharp.ide.completion.util.LtGtInsertHandler;
+import org.mustbe.consulo.csharp.lang.psi.CSharpMethodDeclaration;
+import org.mustbe.consulo.csharp.lang.psi.CSharpReferenceExpression;
+import org.mustbe.consulo.csharp.lang.psi.CSharpUsingList;
+import org.mustbe.consulo.csharp.lang.psi.impl.source.resolve.util.CSharpResolveUtil;
+import org.mustbe.consulo.dotnet.libraryAnalyzer.NamespaceReference;
+import org.mustbe.consulo.dotnet.psi.DotNetGenericParameter;
 import org.mustbe.consulo.dotnet.psi.DotNetQualifiedElement;
+import org.mustbe.consulo.dotnet.psi.DotNetTypeDeclaration;
 import org.mustbe.consulo.dotnet.resolve.DotNetNamespaceAsElement;
+import org.mustbe.consulo.dotnet.resolve.DotNetShortNameSearcher;
+import org.mustbe.consulo.dotnet.resolve.GlobalSearchScopeFilter;
+import org.mustbe.dotnet.msil.decompiler.util.MsilHelper;
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionResult;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.codeInsight.completion.CompletionUtil;
+import com.intellij.codeInsight.completion.InsertHandler;
+import com.intellij.codeInsight.completion.InsertionContext;
+import com.intellij.codeInsight.completion.PrefixMatcher;
 import com.intellij.codeInsight.completion.impl.BetterPrefixMatcher;
 import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.ide.IconDescriptorUpdaters;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Iconable;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Consumer;
+import com.intellij.util.Function;
+import com.intellij.util.Processor;
+import com.intellij.util.indexing.IdFilter;
 
 /**
  * @author VISTALL
@@ -88,7 +117,7 @@ public class CSharpNoVariantsDelegator extends CompletionContributor
 					CSharpCompletionUtil.mayStartClassName(result) &&
 					CSharpCompletionUtil.isClassNamePossible(parameters))
 			{
-				CSharpTypeReferenceCompletionContributor.addTypesForUsing(parameters, result.withPrefixMatcher(tracker
+				addTypesForUsing(parameters, result.withPrefixMatcher(tracker
 						.betterMatcher), holder);
 			}
 		}
@@ -105,7 +134,7 @@ public class CSharpNoVariantsDelegator extends CompletionContributor
 					CSharpCompletionUtil.mayStartClassName(result) &&
 					CSharpCompletionUtil.isClassNamePossible(parameters))
 			{
-				CSharpTypeReferenceCompletionContributor.addTypesForUsing(parameters, result, inheritorsHolder);
+				addTypesForUsing(parameters, result, inheritorsHolder);
 				return;
 			}
 
@@ -116,5 +145,130 @@ public class CSharpNoVariantsDelegator extends CompletionContributor
 		{
 			result.runRemainingContributors(parameters.withInvocationCount(3), true);
 		}
+	}
+
+	public static void addTypesForUsing(final CompletionParameters parameters,
+			final CompletionResultSet result,
+			final InheritorsHolder inheritorsHolder)
+	{
+		final PrefixMatcher matcher = result.getPrefixMatcher();
+		CSharpReferenceExpression parent = (CSharpReferenceExpression) parameters.getPosition().getParent();
+
+		final Project project = parent.getProject();
+		final GlobalSearchScope resolveScope = parent.getResolveScope();
+
+		final DotNetShortNameSearcher shortNameSearcher = DotNetShortNameSearcher.getInstance(project);
+		final IdFilter projectIdFilter = new GlobalSearchScopeFilter(resolveScope);
+
+		final boolean insideUsingList = PsiTreeUtil.getParentOfType(parent, CSharpUsingList.class) != null;
+
+		shortNameSearcher.collectTypeNames(new Processor<String>()
+		{
+			@Override
+			public boolean process(String key)
+			{
+				ProgressManager.checkCanceled();
+
+				if(matcher.prefixMatches(key))
+				{
+					shortNameSearcher.collectTypes(key, resolveScope, projectIdFilter,
+							new Processor<DotNetTypeDeclaration>()
+							{
+								@Override
+								@RequiredReadAction
+								public boolean process(DotNetTypeDeclaration typeDeclaration)
+								{
+									ProgressManager.checkCanceled();
+
+									if(inheritorsHolder.alreadyProcessed(typeDeclaration))
+									{
+										return true;
+									}
+
+									consumeType(parameters, result, insideUsingList, typeDeclaration);
+
+									return true;
+								}
+							});
+				}
+				return true;
+			}
+		}, resolveScope, projectIdFilter);
+	}
+
+	@RequiredReadAction
+	private static void consumeType(final CompletionParameters completionParameters,
+			Consumer<LookupElement> consumer,
+			boolean insideUsingList,
+			DotNetTypeDeclaration someType)
+	{
+		final String parentQName = someType.getPresentableParentQName();
+		if(StringUtil.isEmpty(parentQName))
+		{
+			return;
+		}
+
+		String presentationText = MsilHelper.cutGenericMarker(someType.getName());
+
+		int genericCount = 0;
+		DotNetGenericParameter[] genericParameters = someType.getGenericParameters();
+		if((genericCount = genericParameters.length) > 0)
+		{
+			presentationText += "<" + StringUtil.join(genericParameters, new Function<DotNetGenericParameter, String>()
+			{
+				@Override
+				public String fun(DotNetGenericParameter parameter)
+				{
+					return parameter.getName();
+				}
+			}, ", ");
+			presentationText += ">";
+		}
+
+		String lookupString = insideUsingList ? someType.getPresentableQName() : someType.getName();
+		if(lookupString == null)
+		{
+			return;
+		}
+		lookupString = MsilHelper.cutGenericMarker(lookupString);
+
+		DotNetQualifiedElement targetElementForLookup = someType;
+		CSharpMethodDeclaration methodDeclaration = someType.getUserData(CSharpResolveUtil.DELEGATE_METHOD_TYPE);
+		if(methodDeclaration != null)
+		{
+			targetElementForLookup = methodDeclaration;
+		}
+		LookupElementBuilder builder = LookupElementBuilder.create(targetElementForLookup, lookupString);
+		builder = builder.withPresentableText(presentationText);
+		builder = builder.withIcon(IconDescriptorUpdaters.getIcon(targetElementForLookup,
+				Iconable.ICON_FLAG_VISIBILITY));
+
+		builder = builder.withTypeText(parentQName, true);
+		final InsertHandler<LookupElement> ltGtInsertHandler = genericCount == 0 ? null : LtGtInsertHandler
+				.getInstance(genericCount > 0);
+		if(insideUsingList)
+		{
+			builder = builder.withInsertHandler(ltGtInsertHandler);
+		}
+		else
+		{
+			builder = builder.withInsertHandler(new InsertHandler<LookupElement>()
+			{
+				@Override
+				public void handleInsert(InsertionContext context, LookupElement item)
+				{
+					if(ltGtInsertHandler != null)
+					{
+						ltGtInsertHandler.handleInsert(context, item);
+					}
+
+					new AddUsingAction(completionParameters.getEditor(), context.getFile(),
+							Collections.<NamespaceReference>singleton(new NamespaceReference(parentQName,
+									null))).execute();
+				}
+			});
+		}
+
+		consumer.consume(new ReplaceableTypeLikeLookupElement(builder));
 	}
 }
