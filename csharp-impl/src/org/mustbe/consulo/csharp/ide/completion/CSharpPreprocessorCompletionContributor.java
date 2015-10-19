7d3e94c54bf51c9a2a3783550b746dafa7b9a54f
VISTALL <vistall.valeriy@gmail.com>
2015-10-19 18:28:38 +0300

    some compiletion

--- csharp-impl/src/org/mustbe/consulo/csharp/ide/completion/CSharpPreprocessorCompletionContributor.java
+++ csharp-impl/src/org/mustbe/consulo/csharp/ide/completion/CSharpPreprocessorCompletionContributor.java
@@ -22,7 +22,9 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.jetbrains.annotations.NotNull;
+import org.mustbe.consulo.RequiredReadAction;
 import org.mustbe.consulo.csharp.ide.CSharpLookupElementBuilder;
+import org.mustbe.consulo.csharp.ide.completion.util.SpaceInsertHandler;
 import org.mustbe.consulo.csharp.lang.psi.CSharpMacroDefine;
 import org.mustbe.consulo.csharp.lang.psi.CSharpMacroTokens;
 import org.mustbe.consulo.csharp.lang.psi.impl.light.CSharpLightMacroDefine;
@@ -35,7 +37,10 @@ import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.util.Pair;
 import com.intellij.util.ProcessingContext;
 import com.intellij.util.containers.HashMap;
 
@@ -43,13 +48,49 @@ import com.intellij.util.containers.HashMap;
  * @author VISTALL
  * @since 29.01.15
  */
-public class CSharpMacroReferenceCompletionContributor extends CompletionContributor
+public class CSharpPreprocessorCompletionContributor extends CompletionContributor
 {
-	public CSharpMacroReferenceCompletionContributor()
+	@SuppressWarnings("unchecked")
+	private static final Pair<String, Boolean>[] ourPreprocessorKeywords = new Pair[]{
+			Pair.create("define", Boolean.TRUE),
+			Pair.create("if", Boolean.TRUE),
+			Pair.create("else", Boolean.FALSE),
+			Pair.create("elif", Boolean.TRUE),
+			Pair.create("region", Boolean.TRUE),
+			Pair.create("undef", Boolean.TRUE),
+			Pair.create("endif", Boolean.FALSE),
+			Pair.create("endregion", Boolean.FALSE),
+	};
+
+	public CSharpPreprocessorCompletionContributor()
 	{
-		extend(CompletionType.BASIC, psiElement(CSharpMacroTokens.IDENTIFIER).withParent(CSharpMacroReferenceExpressionImpl.class),
-				new CompletionProvider<CompletionParameters>()
+		extend(CompletionType.BASIC, psiElement().afterLeaf(psiElement(CSharpMacroTokens.SHARP)), new CompletionProvider<CompletionParameters>()
 		{
+			@RequiredReadAction
+			@Override
+			protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result)
+			{
+				for(Pair<String, Boolean> keywordInfo : ourPreprocessorKeywords)
+				{
+					String keyword = keywordInfo.getFirst();
+					Boolean insertSpace = keywordInfo.getSecond();
+
+					LookupElementBuilder builder = LookupElementBuilder.create(keyword);
+					builder = builder.withPresentableText("#" + keyword);
+					if(insertSpace == Boolean.TRUE)
+					{
+						builder = builder.withInsertHandler(SpaceInsertHandler.INSTANCE);
+					}
+					builder = builder.withIcon(AllIcons.Nodes.Tag);
+					builder = builder.bold();
+					result.addElement(builder);
+				}
+			}
+		});
+
+		extend(CompletionType.BASIC, psiElement(CSharpMacroTokens.IDENTIFIER).withParent(CSharpMacroReferenceExpressionImpl.class), new CompletionProvider<CompletionParameters>()
+		{
+			@RequiredReadAction
 			@Override
 			protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result)
 			{
@@ -82,7 +123,6 @@ public class CSharpMacroReferenceCompletionContributor extends CompletionContrib
 						map.put(name, macroDefine);
 					}
 				}
-
 
 				LookupElement[] lookupElements = CSharpLookupElementBuilder.buildToLookupElements(map.values());
 				result.addAllElements(Arrays.asList(lookupElements));
