10f798310e9187dfd0241ded56b528b46086c1d0
VISTALL <vistall.valeriy@gmail.com>
2016-03-07 04:59:05 +0300

    removed debug

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/resolve/CSharpResolveContextUtil.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/resolve/CSharpResolveContextUtil.java
@@ -158,14 +158,7 @@ public class CSharpResolveContextUtil
 		}
 		else
 		{
-			long time = System.currentTimeMillis();
-			CSharpTypeResolveContext context = new CSharpTypeResolveContext(typeDeclaration, genericExtractor, recursiveGuardSet);
-			long l = System.currentTimeMillis() - time;
-			if(l > 10)
-			{
-				System.out.println(l + " time for " + typeDeclaration.getPresentableQName());
-			}
-			return context;
+			return new CSharpTypeResolveContext(typeDeclaration, genericExtractor, recursiveGuardSet);
 		}
 	}
 
