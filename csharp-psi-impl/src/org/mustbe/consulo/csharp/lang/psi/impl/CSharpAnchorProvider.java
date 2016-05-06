da96c8e1a25e7ac8d5fae74fd76eb62751eb427d
VISTALL <vistall.valeriy@gmail.com>
2016-05-06 12:28:28 +0300

    dont try switch msil wrapper

--- csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/CSharpAnchorProvider.java
+++ csharp-psi-impl/src/org/mustbe/consulo/csharp/lang/psi/impl/CSharpAnchorProvider.java
@@ -2,13 +2,15 @@ package org.mustbe.consulo.csharp.lang.psi.impl;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mustbe.consulo.csharp.lang.psi.CSharpIndexMethodDeclaration;
+import org.mustbe.consulo.RequiredReadAction;
 import org.mustbe.consulo.csharp.lang.psi.CSharpConstructorDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpEventDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpFieldDeclaration;
+import org.mustbe.consulo.csharp.lang.psi.CSharpIndexMethodDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpMethodDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpPropertyDeclaration;
 import org.mustbe.consulo.csharp.lang.psi.CSharpTypeDeclaration;
+import org.mustbe.consulo.csharp.lang.psi.impl.msil.MsilElementWrapper;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNameIdentifierOwner;
 import com.intellij.psi.impl.smartPointers.SmartPointerAnchorProvider;
@@ -21,8 +23,13 @@ public class CSharpAnchorProvider implements SmartPointerAnchorProvider
 {
 	@Nullable
 	@Override
+	@RequiredReadAction
 	public PsiElement getAnchor(@NotNull PsiElement element)
 	{
+		if(element instanceof MsilElementWrapper || !element.isPhysical())
+		{
+			return null;
+		}
 		if(element instanceof CSharpTypeDeclaration ||
 				element instanceof CSharpFieldDeclaration ||
 				element instanceof CSharpMethodDeclaration ||
@@ -41,6 +48,6 @@ public class CSharpAnchorProvider implements SmartPointerAnchorProvider
 	@Override
 	public PsiElement restoreElement(@NotNull PsiElement anchor)
 	{
-		return null;
+		return anchor.getParent();
 	}
 }
