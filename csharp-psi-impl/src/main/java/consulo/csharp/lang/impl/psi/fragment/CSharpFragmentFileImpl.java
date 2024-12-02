6908fff8209eb5e3ec48e1539f317cd893bb6aac
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-12-02 14:44:22 +0400

    remove debug

--- csharp-psi-impl/src/main/java/consulo/csharp/lang/impl/psi/fragment/CSharpFragmentFileImpl.java
+++ csharp-psi-impl/src/main/java/consulo/csharp/lang/impl/psi/fragment/CSharpFragmentFileImpl.java
@@ -60,8 +60,6 @@ public class CSharpFragmentFileImpl extends PsiFileImpl implements CSharpCodeFra
 	{
 		super(elementType, contentElementType, provider);
 		myContext = context;
-
-		System.out.println(provider.getContents());
 	}
 
 	@RequiredReadAction
