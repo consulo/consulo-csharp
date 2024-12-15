55aa812e7c8ba35b4a4841ec763e170a57c22385
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-12-16 00:34:29 +0400

    add opens

--- csharp-base-impl/src/main/java/module-info.java
+++ csharp-base-impl/src/main/java/module-info.java
@@ -2,16 +2,17 @@
  * @author VISTALL
  * @since 10-Sep-22
  */
-module consulo.csharp.base.impl
-{
-	// TODO remove in future
-	requires java.desktop;
+module consulo.csharp.base.impl {
+    // TODO remove in future
+    requires java.desktop;
 
-	requires transitive consulo.csharp.api;
-	requires transitive consulo.csharp.psi.impl;
-	requires transitive consulo.dotnet.impl;
-	requires transitive consulo.dotnet.psi.api;
+    requires transitive consulo.csharp.api;
+    requires transitive consulo.csharp.psi.impl;
+    requires transitive consulo.dotnet.impl;
+    requires transitive consulo.dotnet.psi.api;
 
-	exports consulo.csharp.base.compiler;
-	exports consulo.csharp.base.module.extension;
+    exports consulo.csharp.base.compiler;
+    exports consulo.csharp.base.module.extension;
+
+    opens consulo.csharp.base.module.extension to consulo.application.impl;
 }
