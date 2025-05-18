e93201eee82d513cf788e495b0d9d95ba90df830
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-05-18 22:31:56 +0400

    reformat

--- csharp-psi-impl/src/main/java/consulo/csharp/lang/impl/psi/resolve/additionalMembersImpl/OperatorsProvider.java
+++ csharp-psi-impl/src/main/java/consulo/csharp/lang/impl/psi/resolve/additionalMembersImpl/OperatorsProvider.java
@@ -40,8 +40,8 @@ import consulo.language.psi.PsiElement;
 import consulo.language.psi.scope.GlobalSearchScope;
 import consulo.project.Project;
 import consulo.util.collection.ContainerUtil;
-
 import jakarta.annotation.Nonnull;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -52,164 +52,141 @@ import java.util.function.Consumer;
  * @since 26.10.14
  */
 @ExtensionImpl
-public class OperatorsProvider implements CSharpAdditionalMemberProvider
-{
-	@RequiredReadAction
-	@Override
-	public void processAdditionalMembers(@Nonnull DotNetElement element, @Nonnull DotNetGenericExtractor extractor, @Nonnull Consumer<PsiElement> consumer)
-	{
-		if(element instanceof CSharpTypeDeclaration)
-		{
-			Project project = element.getProject();
-			GlobalSearchScope resolveScope = element.getResolveScope();
+public class OperatorsProvider implements CSharpAdditionalMemberProvider {
+    @RequiredReadAction
+    @Override
+    public void processAdditionalMembers(@Nonnull DotNetElement element, @Nonnull DotNetGenericExtractor extractor, @Nonnull Consumer<PsiElement> consumer) {
+        if (element instanceof CSharpTypeDeclaration) {
+            Project project = element.getProject();
+            GlobalSearchScope resolveScope = element.getResolveScope();
 
-			CSharpTypeDeclaration typeDeclaration = (CSharpTypeDeclaration) element;
+            CSharpTypeDeclaration typeDeclaration = (CSharpTypeDeclaration) element;
 
-			CSharpMethodDeclaration methodDeclaration = typeDeclaration.getUserData(CSharpResolveUtil.DELEGATE_METHOD_TYPE);
-			DotNetTypeRef selfTypeRef;
-			if(methodDeclaration != null)
-			{
-				selfTypeRef = new CSharpLambdaTypeRef(element.getProject(), element.getResolveScope(), methodDeclaration, extractor);
-			}
-			else
-			{
-				selfTypeRef = new CSharpTypeRefByTypeDeclaration(typeDeclaration, extractor);
-			}
+            CSharpMethodDeclaration methodDeclaration = typeDeclaration.getUserData(CSharpResolveUtil.DELEGATE_METHOD_TYPE);
+            DotNetTypeRef selfTypeRef;
+            if (methodDeclaration != null) {
+                selfTypeRef = new CSharpLambdaTypeRef(element.getProject(), element.getResolveScope(), methodDeclaration, extractor);
+            }
+            else {
+                selfTypeRef = new CSharpTypeRefByTypeDeclaration(typeDeclaration, extractor);
+            }
 
-			buildOperators(project, resolveScope, selfTypeRef, element, OperatorStubsLoader.INSTANCE.myTypeOperators.get(typeDeclaration.getVmQName()), consumer);
+            buildOperators(project, resolveScope, selfTypeRef, element, OperatorStubsLoader.INSTANCE.myTypeOperators.get(typeDeclaration.getVmQName()), consumer);
 
-			if(typeDeclaration.isEnum())
-			{
-				buildOperators(project, resolveScope, selfTypeRef, element, OperatorStubsLoader.INSTANCE.myEnumOperators, consumer);
-			}
+            if (typeDeclaration.isEnum()) {
+                buildOperators(project, resolveScope, selfTypeRef, element, OperatorStubsLoader.INSTANCE.myEnumOperators, consumer);
+            }
 
-			if(DotNetTypes.System.Nullable$1.equals(typeDeclaration.getVmQName()))
-			{
-				buildNullableOperators(project, resolveScope, selfTypeRef, typeDeclaration, extractor, consumer);
-			}
+            if (DotNetTypes.System.Nullable$1.equals(typeDeclaration.getVmQName())) {
+                buildNullableOperators(project, resolveScope, selfTypeRef, typeDeclaration, extractor, consumer);
+            }
 
-			if(methodDeclaration != null)
-			{
-				buildOperatorsForDelegates(consumer, project, typeDeclaration, selfTypeRef);
-			}
-		}
-	}
+            if (methodDeclaration != null) {
+                buildOperatorsForDelegates(consumer, project, typeDeclaration, selfTypeRef);
+            }
+        }
+    }
 
-	@Nonnull
-	@Override
-	public Target getTarget()
-	{
-		return Target.OPERATOR_METHOD;
-	}
+    @Nonnull
+    @Override
+    public Target getTarget() {
+        return Target.OPERATOR_METHOD;
+    }
 
-	private static void buildOperatorsForDelegates(Consumer<PsiElement> consumer, Project project, CSharpTypeDeclaration typeDeclaration, DotNetTypeRef selfTypeRef)
-	{
-		for(IElementType elementType : new IElementType[]{
-				CSharpTokens.PLUS,
-				CSharpTokens.MINUS
-		})
-		{
-			CSharpLightMethodDeclarationBuilder builder = new CSharpLightMethodDeclarationBuilder(project);
-			builder.setOperator(elementType);
+    private static void buildOperatorsForDelegates(Consumer<PsiElement> consumer, Project project, CSharpTypeDeclaration typeDeclaration, DotNetTypeRef selfTypeRef) {
+        for (IElementType elementType : new IElementType[]{
+            CSharpTokens.PLUS,
+            CSharpTokens.MINUS
+        }) {
+            CSharpLightMethodDeclarationBuilder builder = new CSharpLightMethodDeclarationBuilder(project);
+            builder.setOperator(elementType);
 
-			builder.withParent(typeDeclaration);
+            builder.withParent(typeDeclaration);
 
-			builder.withReturnType(selfTypeRef);
+            builder.withReturnType(selfTypeRef);
 
-			for(int i = 0; i < 2; i++)
-			{
-				CSharpLightParameterBuilder parameterBuilder = new CSharpLightParameterBuilder(project);
-				parameterBuilder.withName("p" + i);
-				parameterBuilder.withTypeRef(selfTypeRef);
-				builder.addParameter(parameterBuilder);
-			}
+            for (int i = 0; i < 2; i++) {
+                CSharpLightParameterBuilder parameterBuilder = new CSharpLightParameterBuilder(project);
+                parameterBuilder.withName("p" + i);
+                parameterBuilder.withTypeRef(selfTypeRef);
+                builder.addParameter(parameterBuilder);
+            }
 
-			consumer.accept(builder);
-		}
-	}
+            consumer.accept(builder);
+        }
+    }
 
-	@Nonnull
-	@RequiredReadAction
-	private DotNetElement[] buildNullableOperators(@Nonnull Project project,
-			@Nonnull GlobalSearchScope resolveScope,
-			@Nonnull DotNetTypeRef selfTypeRef,
-			@Nonnull CSharpTypeDeclaration typeDeclaration,
-			@Nonnull DotNetGenericExtractor extractor,
-			@Nonnull Consumer<PsiElement> consumer)
-	{
-		DotNetGenericParameter[] genericParameters = typeDeclaration.getGenericParameters();
-		if(genericParameters.length == 0)
-		{
-			return DotNetElement.EMPTY_ARRAY;
-		}
-		DotNetGenericParameter genericParameter = genericParameters[0];
+    @Nonnull
+    @RequiredReadAction
+    private DotNetElement[] buildNullableOperators(@Nonnull Project project,
+                                                   @Nonnull GlobalSearchScope resolveScope,
+                                                   @Nonnull DotNetTypeRef selfTypeRef,
+                                                   @Nonnull CSharpTypeDeclaration typeDeclaration,
+                                                   @Nonnull DotNetGenericExtractor extractor,
+                                                   @Nonnull Consumer<PsiElement> consumer) {
+        DotNetGenericParameter[] genericParameters = typeDeclaration.getGenericParameters();
+        if (genericParameters.length == 0) {
+            return DotNetElement.EMPTY_ARRAY;
+        }
+        DotNetGenericParameter genericParameter = genericParameters[0];
 
-		DotNetTypeRef extract = extractor.extract(genericParameter);
-		if(extract == null)
-		{
-			return DotNetElement.EMPTY_ARRAY;
-		}
+        DotNetTypeRef extract = extractor.extract(genericParameter);
+        if (extract == null) {
+            return DotNetElement.EMPTY_ARRAY;
+        }
 
-		DotNetTypeResolveResult typeResolveResult = extract.resolve();
-		PsiElement typeResolveResultElement = typeResolveResult.getElement();
-		if(!(typeResolveResultElement instanceof DotNetTypeDeclaration))
-		{
-			return DotNetElement.EMPTY_ARRAY;
-		}
+        DotNetTypeResolveResult typeResolveResult = extract.resolve();
+        PsiElement typeResolveResultElement = typeResolveResult.getElement();
+        if (!(typeResolveResultElement instanceof DotNetTypeDeclaration)) {
+            return DotNetElement.EMPTY_ARRAY;
+        }
 
-		List<DotNetElement> elements = new ArrayList<DotNetElement>();
+        List<DotNetElement> elements = new ArrayList<DotNetElement>();
 
-		DotNetTypeDeclaration forAddOperatorsElement = (DotNetTypeDeclaration) typeResolveResultElement;
+        DotNetTypeDeclaration forAddOperatorsElement = (DotNetTypeDeclaration) typeResolveResultElement;
 
-		buildOperators(project, resolveScope, selfTypeRef, forAddOperatorsElement, OperatorStubsLoader.INSTANCE.myTypeOperators.get(forAddOperatorsElement.getVmQName()), consumer);
+        buildOperators(project, resolveScope, selfTypeRef, forAddOperatorsElement, OperatorStubsLoader.INSTANCE.myTypeOperators.get(forAddOperatorsElement.getVmQName()), consumer);
 
-		if(forAddOperatorsElement.isEnum())
-		{
-			buildOperators(project, resolveScope, selfTypeRef, forAddOperatorsElement, OperatorStubsLoader.INSTANCE.myEnumOperators, consumer);
-		}
-		return ContainerUtil.toArray(elements, DotNetElement.ARRAY_FACTORY);
-	}
+        if (forAddOperatorsElement.isEnum()) {
+            buildOperators(project, resolveScope, selfTypeRef, forAddOperatorsElement, OperatorStubsLoader.INSTANCE.myEnumOperators, consumer);
+        }
+        return ContainerUtil.toArray(elements, DotNetElement.ARRAY_FACTORY);
+    }
 
-	private static void buildOperators(@Nonnull Project project,
-			@Nonnull GlobalSearchScope resolveScope,
-			@Nonnull DotNetTypeRef selfTypeRef,
-			@Nonnull DotNetElement parent,
-			@Nonnull Collection<OperatorStubsLoader.Operator> operators,
-			@Nonnull Consumer<PsiElement> consumer)
-	{
-		if(operators.isEmpty())
-		{
-			return;
-		}
+    private static void buildOperators(@Nonnull Project project,
+                                       @Nonnull GlobalSearchScope resolveScope,
+                                       @Nonnull DotNetTypeRef selfTypeRef,
+                                       @Nonnull DotNetElement parent,
+                                       @Nonnull Collection<OperatorStubsLoader.Operator> operators,
+                                       @Nonnull Consumer<PsiElement> consumer) {
+        if (operators.isEmpty()) {
+            return;
+        }
 
-		for(OperatorStubsLoader.Operator operator : operators)
-		{
-			if(operator.myOperatorToken == CSharpTokens.IMPLICIT_KEYWORD || operator.myOperatorToken == CSharpTokens.EXPLICIT_KEYWORD)
-			{
-				// ignore see ConversionMethodsProvider
-			}
-			else
-			{
-				CSharpLightMethodDeclarationBuilder temp = new CSharpLightMethodDeclarationBuilder(project);
-				temp.setOperator(operator.myOperatorToken);
+        for (OperatorStubsLoader.Operator operator : operators) {
+            if (operator.myOperatorToken == CSharpTokens.IMPLICIT_KEYWORD || operator.myOperatorToken == CSharpTokens.EXPLICIT_KEYWORD) {
+                // ignore see ConversionMethodsProvider
+            }
+            else {
+                CSharpLightMethodDeclarationBuilder temp = new CSharpLightMethodDeclarationBuilder(project);
+                temp.setOperator(operator.myOperatorToken);
 
-				temp.withParent(parent);
+                temp.withParent(parent);
 
-				temp.withReturnType(operator.myReturnTypeRef == null ? selfTypeRef : new CSharpTypeRefByQName(project, resolveScope, operator.myReturnTypeRef));
+                temp.withReturnType(operator.myReturnTypeRef == null ? selfTypeRef : new CSharpTypeRefByQName(project, resolveScope, operator.myReturnTypeRef));
 
-				int i = 0;
-				for(OperatorStubsLoader.Operator.Parameter parameter : operator.myParameterTypes)
-				{
-					CSharpLightParameterBuilder parameterBuilder = new CSharpLightParameterBuilder(project);
-					parameterBuilder.withName("p" + i);
-					parameterBuilder.withTypeRef(parameter.myTypeRef == null ? selfTypeRef : new CSharpTypeRefByQName(project, resolveScope, parameter.myTypeRef));
+                int i = 0;
+                for (OperatorStubsLoader.Operator.Parameter parameter : operator.myParameterTypes) {
+                    CSharpLightParameterBuilder parameterBuilder = new CSharpLightParameterBuilder(project);
+                    parameterBuilder.withName("p" + i);
+                    parameterBuilder.withTypeRef(parameter.myTypeRef == null ? selfTypeRef : new CSharpTypeRefByQName(project, resolveScope, parameter.myTypeRef));
 
-					temp.addParameter(parameterBuilder);
-					i++;
-				}
+                    temp.addParameter(parameterBuilder);
+                    i++;
+                }
 
-				consumer.accept(temp);
-			}
-		}
-	}
+                consumer.accept(temp);
+            }
+        }
+    }
 }
