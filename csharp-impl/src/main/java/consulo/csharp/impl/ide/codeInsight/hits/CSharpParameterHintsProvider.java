977a264d2702e498f481ab7f818fef2a39c20a4b
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-06-15 16:02:17 +0400

    updated paraemters hint impl

--- csharp-impl/src/main/java/consulo/csharp/impl/ide/codeInsight/hits/CSharpParameterHintsProvider.java
+++ csharp-impl/src/main/java/consulo/csharp/impl/ide/codeInsight/hits/CSharpParameterHintsProvider.java
@@ -19,17 +19,18 @@ import consulo.dotnet.psi.DotNetVariable;
 import consulo.dotnet.psi.resolve.DotNetTypeRef;
 import consulo.dotnet.psi.resolve.DotNetTypeResolveResult;
 import consulo.language.Language;
+import consulo.language.editor.inlay.HintInfo;
 import consulo.language.editor.inlay.InlayInfo;
 import consulo.language.editor.inlay.InlayParameterHintsProvider;
-import consulo.language.editor.inlay.MethodInfo;
 import consulo.language.psi.PsiElement;
 import consulo.language.psi.PsiNamedElement;
 import consulo.language.psi.util.PsiTreeUtil;
+import consulo.localize.LocalizeValue;
 import consulo.util.collection.ContainerUtil;
 import consulo.util.collection.SmartList;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -39,158 +40,139 @@ import java.util.Set;
  * @since 16-Jan-17
  */
 @ExtensionImpl
-public class CSharpParameterHintsProvider implements InlayParameterHintsProvider
-{
-	private static String[] ourDefaultBlacklist = {
-			"*.Write(*)",
-			"*.WriteLine(*)",
-			"System.Func*",
-			"System.Action*",
-			"*.Add(value)",
-			"*.Add(item)",
-			"*.Add(key, value)",
-			"*.TryParse(*, *)"
-	};
+public class CSharpParameterHintsProvider implements InlayParameterHintsProvider {
+    private static Set<String> ourDefaultBlacklist = Set.of(
+        "*.Write(*)",
+        "*.WriteLine(*)",
+        "System.Func*",
+        "System.Action*",
+        "*.Add(value)",
+        "*.Add(item)",
+        "*.Add(key, value)",
+        "*.TryParse(*, *)"
+    );
 
-	@Nonnull
-	@Override
-	@RequiredReadAction
-	public List<InlayInfo> getParameterHints(@Nonnull PsiElement psiElement)
-	{
-		if(psiElement instanceof CSharpExpressionWithOperatorImpl || psiElement instanceof CSharpOperatorReferenceImpl)
-		{
-			return Collections.emptyList();
-		}
+    @Nonnull
+    @Override
+    @RequiredReadAction
+    public List<InlayInfo> getParameterHints(@Nonnull PsiElement psiElement) {
+        if (psiElement instanceof CSharpExpressionWithOperatorImpl || psiElement instanceof CSharpOperatorReferenceImpl) {
+            return Collections.emptyList();
+        }
 
-		if(psiElement instanceof CSharpCallArgumentListOwner)
-		{
-			PsiElement callable = ((CSharpCallArgumentListOwner) psiElement).resolveToCallable();
-			if(!(callable instanceof CSharpMethodDeclaration) && !(callable instanceof CSharpConstructorDeclaration) && !(callable instanceof DotNetVariable))
-			{
-				return Collections.emptyList();
-			}
+        if (psiElement instanceof CSharpCallArgumentListOwner) {
+            PsiElement callable = ((CSharpCallArgumentListOwner) psiElement).resolveToCallable();
+            if (!(callable instanceof CSharpMethodDeclaration) && !(callable instanceof CSharpConstructorDeclaration) && !(callable instanceof DotNetVariable)) {
+                return Collections.emptyList();
+            }
 
-			List<InlayInfo> list = new SmartList<>();
-			CSharpCallArgument[] callArguments = ((CSharpCallArgumentListOwner) psiElement).getCallArguments();
+            List<InlayInfo> list = new SmartList<>();
+            CSharpCallArgument[] callArguments = ((CSharpCallArgumentListOwner) psiElement).getCallArguments();
 
-			List<NCallArgument> argumentList = buildCallArguments(callArguments, callable, callable);
+            List<NCallArgument> argumentList = buildCallArguments(callArguments, callable, callable);
 
-			for(NCallArgument nCallArgument : argumentList)
-			{
-				if(nCallArgument instanceof NNamedCallArgument || nCallArgument instanceof NErrorCallArgument || nCallArgument instanceof NParamsCallArgument)
-				{
-					continue;
-				}
+            for (NCallArgument nCallArgument : argumentList) {
+                if (nCallArgument instanceof NNamedCallArgument || nCallArgument instanceof NErrorCallArgument || nCallArgument instanceof NParamsCallArgument) {
+                    continue;
+                }
 
-				PsiElement parameterElement = nCallArgument.getParameterElement();
-				DotNetExpression argumentExpression = nCallArgument.getCallArgument() == null ? null : nCallArgument.getCallArgument().getArgumentExpression();
-				if(!CSharpConstantUtil.isConstant(argumentExpression) || parameterElement == null)
-				{
-					continue;
-				}
+                PsiElement parameterElement = nCallArgument.getParameterElement();
+                DotNetExpression argumentExpression = nCallArgument.getCallArgument() == null ? null : nCallArgument.getCallArgument().getArgumentExpression();
+                if (!CSharpConstantUtil.isConstant(argumentExpression) || parameterElement == null) {
+                    continue;
+                }
 
-				String parameterName = nCallArgument.getParameterName();
-				if(parameterName == null)
-				{
-					continue;
-				}
+                String parameterName = nCallArgument.getParameterName();
+                if (parameterName == null) {
+                    continue;
+                }
 
-				list.add(new InlayInfo(parameterName, argumentExpression.getTextOffset()));
-			}
-			return list;
-		}
-		return Collections.emptyList();
-	}
+                list.add(new InlayInfo(parameterName, argumentExpression.getTextOffset()));
+            }
+            return list;
+        }
+        return Collections.emptyList();
+    }
 
-	@RequiredReadAction
-	private static List<NCallArgument> buildCallArguments(@Nonnull CSharpCallArgument[] callArguments, @Nonnull PsiElement callable, @Nonnull PsiElement scopedElement)
-	{
-		if(callable instanceof DotNetVariable)
-		{
-			DotNetTypeRef ref = ((DotNetVariable) callable).toTypeRef(true);
-			DotNetTypeResolveResult resolve = ref.resolve();
-			if(resolve instanceof CSharpLambdaResolveResult)
-			{
-				return NCallArgumentBuilder.buildCallArguments(scopedElement.getProject(), callArguments, ((CSharpLambdaResolveResult) resolve).getParameterInfos(), scopedElement.getResolveScope());
-			}
-		}
-		else if(callable instanceof DotNetParameterListOwner)
-		{
-			return NCallArgumentBuilder.buildCallArguments(callArguments, (DotNetParameterListOwner) callable, scopedElement.getResolveScope());
-		}
+    @RequiredReadAction
+    private static List<NCallArgument> buildCallArguments(@Nonnull CSharpCallArgument[] callArguments, @Nonnull PsiElement callable, @Nonnull PsiElement scopedElement) {
+        if (callable instanceof DotNetVariable) {
+            DotNetTypeRef ref = ((DotNetVariable) callable).toTypeRef(true);
+            DotNetTypeResolveResult resolve = ref.resolve();
+            if (resolve instanceof CSharpLambdaResolveResult) {
+                return NCallArgumentBuilder.buildCallArguments(scopedElement.getProject(), callArguments, ((CSharpLambdaResolveResult) resolve).getParameterInfos(), scopedElement.getResolveScope());
+            }
+        }
+        else if (callable instanceof DotNetParameterListOwner) {
+            return NCallArgumentBuilder.buildCallArguments(callArguments, (DotNetParameterListOwner) callable, scopedElement.getResolveScope());
+        }
 
-		return Collections.emptyList();
-	}
+        return Collections.emptyList();
+    }
 
-	@Nullable
-	@Override
-	@RequiredReadAction
-	public MethodInfo getMethodInfo(@Nonnull PsiElement call)
-	{
-		PsiElement callable;
-		if(call instanceof CSharpCallArgumentListOwner)
-		{
-			callable = ((CSharpCallArgumentListOwner) call).resolveToCallable();
-			if(!(callable instanceof CSharpMethodDeclaration) && !(callable instanceof DotNetVariable) && !(callable instanceof CSharpConstructorDeclaration))
-			{
-				return null;
-			}
-		}
-		else
-		{
-			return null;
-		}
+    @Nullable
+    @Override
+    @RequiredReadAction
+    public HintInfo.MethodInfo getHintInfo(@Nonnull PsiElement call) {
+        PsiElement callable;
+        if (call instanceof CSharpCallArgumentListOwner) {
+            callable = ((CSharpCallArgumentListOwner) call).resolveToCallable();
+            if (!(callable instanceof CSharpMethodDeclaration) && !(callable instanceof DotNetVariable) && !(callable instanceof CSharpConstructorDeclaration)) {
+                return null;
+            }
+        }
+        else {
+            return null;
+        }
 
-		String name = null;
-		CSharpSimpleParameterInfo[] params = CSharpSimpleParameterInfo.EMPTY_ARRAY;
-		if(callable instanceof DotNetVariable)
-		{
-			DotNetTypeRef ref = ((DotNetVariable) callable).toTypeRef(true);
-			DotNetTypeResolveResult resolve = ref.resolve();
-			if(resolve instanceof CSharpLambdaResolveResult)
-			{
-				name = ((CSharpLambdaResolveResult) resolve).getTarget().getPresentableQName();
-				params = ((CSharpLambdaResolveResult) resolve).getParameterInfos();
-			}
-			else
-			{
-				return null;
-			}
-		}
-		else if(callable instanceof CSharpMethodDeclaration || callable instanceof CSharpConstructorDeclaration)
-		{
-			params = ((CSharpSimpleLikeMethodAsElement) callable).getParameterInfos();
+        String name = null;
+        CSharpSimpleParameterInfo[] params = CSharpSimpleParameterInfo.EMPTY_ARRAY;
+        if (callable instanceof DotNetVariable) {
+            DotNetTypeRef ref = ((DotNetVariable) callable).toTypeRef(true);
+            DotNetTypeResolveResult resolve = ref.resolve();
+            if (resolve instanceof CSharpLambdaResolveResult) {
+                name = ((CSharpLambdaResolveResult) resolve).getTarget().getPresentableQName();
+                params = ((CSharpLambdaResolveResult) resolve).getParameterInfos();
+            }
+            else {
+                return null;
+            }
+        }
+        else if (callable instanceof CSharpMethodDeclaration || callable instanceof CSharpConstructorDeclaration) {
+            params = ((CSharpSimpleLikeMethodAsElement) callable).getParameterInfos();
 
-			CSharpTypeDeclaration declaration = PsiTreeUtil.getParentOfType(callable, CSharpTypeDeclaration.class);
-			if(declaration != null)
-			{
-				name = declaration.getPresentableQName() + ".";
-			}
-			name += ((PsiNamedElement) callable).getName();
-		}
+            CSharpTypeDeclaration declaration = PsiTreeUtil.getParentOfType(callable, CSharpTypeDeclaration.class);
+            if (declaration != null) {
+                name = declaration.getPresentableQName() + ".";
+            }
+            name += ((PsiNamedElement) callable).getName();
+        }
 
-		List<String> paramNames = ContainerUtil.map(params, it -> it.getNotNullName());
-		return new MethodInfo(name, paramNames);
-	}
+        List<String> paramNames = ContainerUtil.map(params, CSharpSimpleParameterInfo::getNotNullName);
+        return new HintInfo.MethodInfo(name, paramNames);
+    }
 
-	@Nonnull
-	@Override
-	public String getInlayPresentation(@Nonnull String inlayText)
-	{
-		return inlayText + "=";
-	}
+    @Nonnull
+    @Override
+    public String getInlayPresentation(@Nonnull String inlayText) {
+        return inlayText + "=";
+    }
 
-	@Nonnull
-	@Override
-	public Set<String> getDefaultBlackList()
-	{
-		return Set.of(ourDefaultBlacklist);
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getPreviewFileText() {
+        return LocalizeValue.of();
+    }
 
-	@Nonnull
-	@Override
-	public Language getLanguage()
-	{
-		return CSharpLanguage.INSTANCE;
-	}
+    @Nonnull
+    @Override
+    public Set<String> getDefaultBlackList() {
+        return ourDefaultBlacklist;
+    }
+
+    @Nonnull
+    @Override
+    public Language getLanguage() {
+        return CSharpLanguage.INSTANCE;
+    }
 }
