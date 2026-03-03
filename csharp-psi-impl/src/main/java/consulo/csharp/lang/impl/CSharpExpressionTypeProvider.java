7eb40ee159d018b8065b588da112e1df82dcbd8a
UNV <nikolay@yurchenko.su>
2026-03-04 02:54:57 +0300

    Replacing StringUtil.escapeXml with more optimized XmlStringUtil.escapeText. Some refactoring.

--- csharp-psi-impl/src/main/java/consulo/csharp/lang/impl/CSharpExpressionTypeProvider.java
+++ csharp-psi-impl/src/main/java/consulo/csharp/lang/impl/CSharpExpressionTypeProvider.java
@@ -13,21 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package consulo.csharp.lang.impl;
 
 import consulo.annotation.access.RequiredReadAction;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.csharp.lang.CSharpLanguage;
-import consulo.csharp.lang.psi.CSharpReferenceExpression;
 import consulo.csharp.lang.impl.psi.CSharpTypeRefPresentationUtil;
 import consulo.csharp.lang.impl.psi.source.CSharpMethodCallExpressionImpl;
+import consulo.csharp.lang.psi.CSharpReferenceExpression;
 import consulo.dotnet.psi.DotNetExpression;
 import consulo.language.Language;
 import consulo.language.editor.ExpressionTypeProvider;
 import consulo.language.psi.PsiElement;
 import consulo.language.psi.util.PsiTreeUtil;
-import consulo.util.lang.StringUtil;
+import consulo.util.lang.xml.XmlStringUtil;
 import jakarta.annotation.Nonnull;
 
 import java.util.List;
@@ -37,44 +36,37 @@ import java.util.List;
  * @since 05/05/2021
  */
 @ExtensionImpl
-public class CSharpExpressionTypeProvider extends ExpressionTypeProvider<DotNetExpression>
-{
-	@Nonnull
-	@Override
-	@RequiredReadAction
-	public String getInformationHint(@Nonnull DotNetExpression expression)
-	{
-		return StringUtil.escapeXmlEntities(CSharpTypeRefPresentationUtil.buildText(expression.toTypeRef(true)));
-	}
+public class CSharpExpressionTypeProvider extends ExpressionTypeProvider<DotNetExpression> {
+    @Nonnull
+    @Override
+    @RequiredReadAction
+    public String getInformationHint(@Nonnull DotNetExpression expression) {
+        return XmlStringUtil.escapeText(CSharpTypeRefPresentationUtil.buildText(expression.toTypeRef(true)));
+    }
 
-	@Nonnull
-	@Override
-	public String getErrorHint()
-	{
-		return "Expression not found";
-	}
+    @Nonnull
+    @Override
+    public String getErrorHint() {
+        return "Expression not found";
+    }
 
-	@Nonnull
-	@Override
-	public List<DotNetExpression> getExpressionsAt(@Nonnull PsiElement psiElement)
-	{
-		DotNetExpression expression = PsiTreeUtil.getParentOfType(psiElement, DotNetExpression.class);
-		if(expression instanceof CSharpReferenceExpression && expression.getParent() instanceof CSharpMethodCallExpressionImpl call)
-		{
-			expression = call;
-		}
+    @Nonnull
+    @Override
+    public List<DotNetExpression> getExpressionsAt(@Nonnull PsiElement psiElement) {
+        DotNetExpression expression = PsiTreeUtil.getParentOfType(psiElement, DotNetExpression.class);
+        if (expression instanceof CSharpReferenceExpression && expression.getParent() instanceof CSharpMethodCallExpressionImpl call) {
+            expression = call;
+        }
 
-		if(expression != null)
-		{
-			return List.of(expression);
-		}
-		return List.of();
-	}
+        if (expression != null) {
+            return List.of(expression);
+        }
+        return List.of();
+    }
 
-	@Nonnull
-	@Override
-	public Language getLanguage()
-	{
-		return CSharpLanguage.INSTANCE;
-	}
+    @Nonnull
+    @Override
+    public Language getLanguage() {
+        return CSharpLanguage.INSTANCE;
+    }
 }
